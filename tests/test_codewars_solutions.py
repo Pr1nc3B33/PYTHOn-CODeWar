2645592db17b6b0b291dc7292b088bac9aa58ac0
Benny Bailey <B3nnyB@B3nnyB.local>
2026-03-06 13:05:56 -0800

    Add pytest unit tests for codewars solutions

---
+++ tests/test_codewars_solutions.py
@@ -0,0 +1,45 @@
+import pytest
+
+from codewars_solutions import even_or_odd, number_to_string, no_space, get_count
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [(2, "Even"), (7, "Odd"), (0, "Even"), (-3, "Odd"), (-4, "Even")],
+)
+def test_even_or_odd(value, expected):
+    assert even_or_odd(value) == expected
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [(67, "67"), (0, "0"), (-12, "-12")],
+)
+def test_number_to_string(value, expected):
+    assert number_to_string(value) == expected
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ("8 j 8   mBliB8g  imjB8B8  jl  B", "8j8mBliB8gimjB8B8jlB"),
+        (" no spaces ", "nospaces"),
+        ("", ""),
+    ],
+)
+def test_no_space(value, expected):
+    assert no_space(value) == expected
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ("abracadabra", 5),
+        ("hello world", 3),
+        ("my pyx", 0),
+        ("aeiou", 5),
+        ("", 0),
+    ],
+)
+def test_get_count(value, expected):
+    assert get_count(value) == expected
